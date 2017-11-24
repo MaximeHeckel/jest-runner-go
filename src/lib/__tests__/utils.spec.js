7db58d7e81c0e0ee7855454e620a53e1b409e889
Maxime Heckel <heckelmaxime@gmail.com>
2017-11-23 20:44:46 -0800

    fix duplicate failure when a test in a given package is failing

--- src/lib/__tests__/utils.spec.js
+++ src/lib/__tests__/utils.spec.js
@@ -1,10 +1,14 @@
 import {
   parseGoOutput,
   toTestResult,
+  pathContainsFailedTest,
 } from '../utils';
 
 
 const path = 'github.com/MaximeHeckel/go-test-runner/example/stringutil';
+const testFile1 = 'github.com/MaximeHeckel/go-test-runner/example/stringutil2/reverse3_test.go';
+const testFile2 = 'github.com/MaximeHeckel/go-test-runner/example/stringutil2/reverse2_test.go';
+const mockedOutputLine = '      reverse3_test.go:14:failed test'
 const start = 1510292283895;
 const output = [ 'ok  \tgithub.com/MaximeHeckel/go-test-runner/example/stringutil\t0.007s','' ];
 
@@ -26,5 +30,15 @@ describe('parseGoOutput', () => {
 describe('toTestResult', () => {
   it('returns a properly formatted test result', () => {
     expect(toTestResult(test)).toMatchSnapshot();
+  });
+});
+
+describe('pathContainsFailedTest', () => {
+  it('returns true when the line contains the file name present in the relative path of the test file', () => {
+    expect(pathContainsFailedTest(mockedOutputLine, testFile1)).toBe(true);
+  });
+
+  it('returns false when the line doesn\'t contain the file name present in the relative path of the test file', () => {
+    expect(pathContainsFailedTest(mockedOutputLine, testFile2)).toBe(false);
   });
 });
